2d59ac43a4ce63b2aff03a3274895939133b45d1
jamboooooo <hisllam35@gmail.com>
2021-07-08 14:49:22 +0300

    third task complete

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import Base from "./Base";
+
+function App() {
+    return <Base/>
+}
+
+export default App
---
+++ src/Base.js
@@ -0,0 +1,8 @@
+function Base() {
+    return <>
+        <h1>Hello, world</h1>
+        <img src="https://im0-tub-ru.yandex.net/i?id=541acae4aa01a1d3a760bc7e4bf046df&n=13" alt="image" />
+    </>
+}
+
+export default Base
